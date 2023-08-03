7dcc95fe984f0db949360021e1d9d2b36f4d39ac
Jennifer Grange <jenny@prefect.io>
2023-08-03 14:09:58 -0400

    comment error

--- subFlowRetry.py
+++ subFlowRetry.py
@@ -2,7 +2,7 @@ from prefect import flow
 
 @flow
 def sub_flow():
-    raise Exception("Error") #COMMENT THIS CODE LATER
+    # raise Exception("Error") #COMMENT THIS CODE LATER
     return
 
 @flow
