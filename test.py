e6d65c9473dbb79e23d79a1ea48a372c6e468a43
Jennifer Grange <jenny@prefect.io>
2023-11-28 11:28:47 -0500

    json1

---
+++ test.py
@@ -0,0 +1,64 @@
+{
+    "start_time": "2023-11-28T16:23:19.528563+00:00",
+    "end_time": "2023-11-28T16:23:20.385839+00:00",
+    "root_node_ids": [
+        "48c47e9a-7718-4249-8246-d933e7f1acdb",
+        "1d1f9d98-c2ab-4e21-ba1a-c7df3c1a5f94",
+        "bf31360b-ec99-4389-84a6-e6d4dae3730f",
+        "cd55e302-d79f-4ae7-842d-6695c64fa5b2"
+    ],
+    "nodes": [
+        [
+            "48c47e9a-7718-4249-8246-d933e7f1acdb",
+            {
+                "kind": "task-run",
+                "id": "48c47e9a-7718-4249-8246-d933e7f1acdb",
+                "label": "markdown_task-0",
+                "state_type": "COMPLETED",
+                "start_time": "2023-11-28T16:23:19.638345+00:00",
+                "end_time": "2023-11-28T16:23:19.748117+00:00",
+                "parents": [],
+                "children": []
+            }
+        ],
+        [
+            "1d1f9d98-c2ab-4e21-ba1a-c7df3c1a5f94",
+            {
+                "kind": "task-run",
+                "id": "1d1f9d98-c2ab-4e21-ba1a-c7df3c1a5f94",
+                "label": "my_first_link_task-0",
+                "state_type": "COMPLETED",
+                "start_time": "2023-11-28T16:23:19.835901+00:00",
+                "end_time": "2023-11-28T16:23:19.931318+00:00",
+                "parents": [],
+                "children": []
+            }
+        ],
+        [
+            "bf31360b-ec99-4389-84a6-e6d4dae3730f",
+            {
+                "kind": "task-run",
+                "id": "bf31360b-ec99-4389-84a6-e6d4dae3730f",
+                "label": "my_second_link_task-0",
+                "state_type": "COMPLETED",
+                "start_time": "2023-11-28T16:23:20.010897+00:00",
+                "end_time": "2023-11-28T16:23:20.128369+00:00",
+                "parents": [],
+                "children": []
+            }
+        ],
+        [
+            "cd55e302-d79f-4ae7-842d-6695c64fa5b2",
+            {
+                "kind": "task-run",
+                "id": "cd55e302-d79f-4ae7-842d-6695c64fa5b2",
+                "label": "my_table_task-0",
+                "state_type": "COMPLETED",
+                "start_time": "2023-11-28T16:23:20.218650+00:00",
+                "end_time": "2023-11-28T16:23:20.331930+00:00",
+                "parents": [],
+                "children": []
+            }
+        ]
+    ]
+}
