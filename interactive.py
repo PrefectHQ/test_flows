24910ae1591efdddf7205f8005f7a6667cccd28e
Jennifer Grange <jenny@prefect.io>
2024-02-08 10:35:59 -0500

    add interactive flow

--- interactive.py
+++ interactive.py
@@ -5,9 +5,11 @@ class UserName(RunInput):
     name: str
 
 @flow(log_prints=True)
-async def greet_user():
-    user_input = await pause_flow_run(
+def greet_user():
+    user_input = pause_flow_run(
         wait_for_input=UserName
     )
     print(f"Hello, {user_input.name}!")
 
+if __name__ == "__main__":
+    greet_user()
